6e7219a17f09de6a3cea682b0fbfe395f6abbe2a
Álisson Pereira Ferreira <113950309+alissonpef@users.noreply.github.com>
2024-12-15 23:29:41 -0300

    Delete Trena_Digital/build directory

--- Trena_Digital/build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/alisson/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- Trena_Digital/build/esp-idf/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
+++
@@ -1,1 +0,0 @@
-/home/alisson/esp/esp-idf/components/mbedtls/mbedtls/library/psa_crypto_driver_wrappers_no_static.c
